eab8123159c95c9a1b4180e10765d17958c3b738
Kevin Pleitez <pleitez95geova@gmail.com>
2017-02-23 12:48:56 -0600

    archivos de proyecto

---
+++ AccesoDatosMYSQL/DBConexion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace AccesoDatosMYSQL
+{
+    public sealed class DBConexion
+    {
+        private static string server = "127.0.0.1";
+        private static string database = "aerolinea";
+        private static string id = "root";
+        private static string password = "admin";
+
+        private static string parametros = "Server=" + server + ";Database=" + database + ";Uid=" + id + ";Pwd=" + password;
+
+        private static MySqlConnection connection;
+
+        public static MySqlConnection Connection {
+            get {
+                    if (connection == null)
+                    {
+                        LazyInitializer.EnsureInitialized(ref connection, CreateConnection);
+                    }
+                    return connection;
+            }
+
+        }
+
+        public static MySqlConnection CreateConnection()
+        {
+            var con = new MySqlConnection(parametros);
+            con.Open();
+            return con;
+        }
+
+        public static void CloseConnection() {
+            if (connection != null) {
+
+                connection.Close();
+                connection.Dispose();
+                connection = null;
+            
+            }
+        }
+
+
+         
+        
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccesoDatosMYSQL;
+using MySql.Data.MySqlClient;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           MySqlCommand cmd =  DBConexion.Connection.CreateCommand();
+           cmd.CommandText = "select * from aeropuertos";
+           MySqlDataReader data =  cmd.ExecuteReader();
+           if (data.HasRows)
+           {
+               while (data.Read())
+               {
+                   Console.WriteLine("{0}\t{1}", data.GetInt32(0),
+                      data.GetString(1));
+               }
+           }
+
+           Console.ReadKey();
+           
+        }
+    }
+}
