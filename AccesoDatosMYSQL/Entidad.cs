6fadd782682d7c95c60283b2c92142cee160aab9
Kevin Pleitez <pleitez95geova@gmail.com>
2017-02-23 17:00:08 -0600

    generacion de selects terminada

---
+++ AccesoDatosMYSQL/Entidad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatosMYSQL
+{
+    public abstract class Entidad
+    {
+        public bool autoincrement = false;
+        public string primary_key = "id";
+    }
+}
